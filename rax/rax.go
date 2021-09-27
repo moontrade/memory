9141ed96d6e766bc88208d5865b953bb855c1cee
Clay Molocznik <info@moontrade.io>
2021-09-26 19:18:19 -0600

    linux rax

--- rax/rax.go
+++ rax/rax.go
@@ -3,10 +3,10 @@ package rax
 /*
 #cgo darwin,amd64 CFLAGS: -I${SRCDIR}/src
 #cgo darwin,amd64 LDFLAGS: -Wl,-rpath,${SRCDIR}/lib/darwin_amd64 -L${SRCDIR}/lib/darwin_amd64
-#cgo darwin,amd64 LDFLAGS: -lrax
+#cgo darwin,amd64 LDFLAGS: -lrax -ldl -lc -lm
 #cgo linux,amd64 CFLAGS: -I${SRCDIR}/src
 #cgo linux,amd64 LDFLAGS: -Wl,-rpath,${SRCDIR}/lib/linux_amd64 -L${SRCDIR}/lib/linux_amd64
-#cgo linux,amd64 LDFLAGS: -lrax
+#cgo linux,amd64 LDFLAGS: -lrax -ldl -lc -lm
 #cgo linux,amd64 CFLAGS: -D_GNU_SOURCE
 #include "rax.h"
 */
